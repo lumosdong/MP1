1c1ac6eea9210aec071b860347595c30b8841063
Lumos <1750650798@qq.com>
2025-07-11 14:04:25 +0800

    update:name and headline

--- src/config/infoConfig.ts
+++ src/config/infoConfig.ts
@@ -3,12 +3,12 @@ export * from './education'
 export * from './career'
 
 // personal info
-export const name = 'Derek Hu'
-export const headline = 'Teacher at Shanghai Soong Ching Ling School.'
+export const name = 'LumosDong'
+export const headline = 'Study in Fuzhou University.'
 export const introduction =
   "Coucou Les Amis! My name is Derek Hu. I'm from Hubei and an alumnus of BUPT, UChicago, and Tsinghua. I’ve been teaching Computer Science at SCLS since 2020."
-export const email = 'tong.hu@scls-sh.org'
-export const githubUsername = 'derekhut'
+export const email = '1750650798@qq.com'
+export const githubUsername = 'LumosDong'
 
 // about page
 export const aboutMeHeadline = 'Who Are You and Why Should I Care?'
